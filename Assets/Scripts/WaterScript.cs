8b46289a889c77abd3150bd6e85a956135451fae
DominikHess <73423350+DominikHess@users.noreply.github.com>
2022-01-10 14:42:16 +0100

    Add waves to the background

--- Assets/Scripts/WaterScript.cs
+++ Assets/Scripts/WaterScript.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class WaterScript : MonoBehaviour
 {
+    private float yPos;
+    private float waveSpeed;
+
+    void Start()
+    {
+        if (gameObject.name == "Waves_front_1")
+        {
+            yPos = -4.75f;
+            waveSpeed = -0.125f;
+        }
+        if (gameObject.name == "Waves_front_2")
+        {
+            yPos = -4.7f;
+            waveSpeed = -0.11f;
+        }
+        if (gameObject.name == "Waves_back_1")
+        {
+            yPos = -3f;
+            waveSpeed = 0.1f;
+        }
+        if (gameObject.name == "Waves_back_2")
+        {
+            yPos = -2.8f;
+            waveSpeed = 0.05f;
+        }
+        if (gameObject.name == "Waves_back_3")
+        {
+            yPos = -2f;
+            waveSpeed = 0.027f;
+        }
+    }
+
     private void FixedUpdate()
     {
-        float y = Mathf.PingPong(Time.time * 0.2f, 0.7f) - 4.75f;
-
-      //  float y = Mathf.PingPong(Time.time * speed, 1) * 6 - 3;
-
+        float y = Mathf.PingPong(Time.time * waveSpeed, 0.7f) + yPos;
         transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 }
